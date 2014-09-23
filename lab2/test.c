fe19a7eb152e3b4ed4c1469794d85da5b9c5692f
Rob Johansen <robert_johans22@hotmail.com>
2014-09-22 22:18:42 -0600

    Commit some progress on ps4

--- lab2/test.c
+++ lab2/test.c
@@ -1,9 +1,46 @@
 #include <stdio.h>
 
+typedef enum {MODE_A, MODE_B, MODE_C, MODE_D, MODE_E} mode_t;
+
 int add(int x, int y)
 {
 	printf("Adding numbers %d and %d\n", x, y);
 	return x + y;
+}
+
+int switch3(int *p1, int *p2, mode_t action)
+{
+	int result = 0;
+	switch (action) {
+	case MODE_A:
+		result = *p1;
+		*p1 = *p2;
+		break;
+	case MODE_B:
+		result  = *p2;
+		result += *p1;
+		*p2 = result;
+		break;
+	case MODE_C:
+		*p2 = 15;
+		result = *p1;
+		break;
+	case MODE_D:
+		*p2 = *p1;
+	case MODE_E:
+		result = 17;
+	}
+	return result;
+}
+
+int loop(int x, int n)
+{
+	int result = -1;
+	int mask;
+	for (mask = 1; mask != 0; mask = mask << n) {
+		result ^= mask & x;
+	}
+	return result;
 }
 
 int main()
