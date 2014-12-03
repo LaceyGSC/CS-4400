f04d24c6ab177436e63a4ca67c891a75db7a3b50
Rob Johansen <johansen@lab1-22.eng.utah.edu>
2014-12-02 21:20:09 -0700

    Commit the initial mm.c file with my name and a .gitignore file.

--- lab6/mm.c
+++ lab6/mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "Polaris",
     /* First member's full name */
-    "Harry Bovik",
+    "Rob Johansen",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "rob.johansen@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
